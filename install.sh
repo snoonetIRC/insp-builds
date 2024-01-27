7fa8e57fdad837ed15dc839e47c2b192de5cec38
linuxdaemon <linuxdaemon.irc@gmail.com>
2024-01-26 19:04:13 -0500

    Update build infra to new versinos

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -e
 
-# Set this to the version of InspIRCd to install
-INSTALL_VERSION="3.8.1"
-
-BASE_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-BUILD_PREFIX="${BASE_DIR}/run"
+BASE_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
 vendor_extra() {
     ./configure --enable-extra "${1}"
@@ -22,10 +18,7 @@ apply_patch() {
 }
 
 apply_patches() {
-    # Fixes a bug in the SHA1 module which prevents us from having WebSocket listeners.
-    apply_patch "https://github.com/inspircd/inspircd/commit/612f4fbe0e98d4bfe5c5527ef2e8153a6600e090.patch"
-    # Fixes a security vulnerability in core_user
-    apply_patch "https://github.com/inspircd/inspircd/commit/6fbe4dd791e62005c5243762cf6bd01b48ed9d73.patch"
+    echo "Applying patches";
 }
 
 install_extras() {
@@ -46,29 +39,24 @@ install_extras() {
     third_extra 'm_totp'
     third_extra 'm_globalmessageflood'
     third_extra 'm_slowmode'
-    third_extra 'm_telegraf'
 }
 
 install() {
     version="${1}"
     version_name="inspircd-${version}"
     url="https://codeload.github.com/inspircd/inspircd/tar.gz/v${version}"
-    [ -d "${BASE_DIR}/${version_name}" ] && rm -r "${BASE_DIR:?}/${version_name}"
+    [ -d "${BASE_DIR:?}/${version_name}" ] && rm -r "${BASE_DIR:?}/${version_name}"
     wget -O - "${url}" | tar zx
-    cd "${BASE_DIR}/${version_name}"
+    cd "${BASE_DIR:?}/${version_name}"
     install_extras
     apply_patches
 
     export CXXFLAGS="-std=c++11"
-    ./configure --disable-auto-extras --prefix "${BUILD_PREFIX}"
+    ./configure --disable-auto-extras --prefix "${BASE_DIR:?}/run"
     make clean && make --jobs "$(nproc)" && make install
 }
 
-if [ $# -ne 0 ]; then
-    VERSION="${1}"
-else
-    VERSION="${INSTALL_VERSION}"
-fi
+VERSION="${1}"
 
 if [ "${VERSION}""x" = "x" ]; then
     echo "No install version specified, exiting..."
